c17bfc331d86137990dbe513bcff0e599be940bb
Kalu-44 <lukakalaba07@gmail.com>
2026-06-02 01:13:05 +0200

    Update URL in BazaLinkova class constructor

--- baza.py
+++ baza.py
@@ -1,7 +1,7 @@
 class BazaLinkova:
 
     def __init__(self):
-        self.URL = "https://www.reddit.com/"
+        self.URL = "https://oet.etf.bg.ac.rs/OET_kolokvijum_2.pdf"
         self.FlagName = ""
 
     def getUrl(self):
