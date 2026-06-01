92b940c8e42b5b3a50008a7ebf5d48820880a6e8
Kalu-44 <lukakalaba07@gmail.com>
2026-06-01 23:44:36 +0100

    baza

---
+++ baza.py
@@ -0,0 +1,11 @@
+class BazaLinkova:
+
+    def __init__(self):
+        self.URL = ""
+        self.FlagName = ""
+
+    def getUrl(self):
+        return self.URL
+
+    def getFlag(self):
+        return self.FlagName
